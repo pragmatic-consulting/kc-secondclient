309a3e152537d65de62d73a3fe03bb52c1eebb41
Alae Es-saki <alaeessaki@gmail.com>
2021-08-05 12:26:22 +0100

    changing path

--- src/main/java/com/pragmatic/kc/security/KeycloakConfig.java
+++ src/main/java/com/pragmatic/kc/security/KeycloakConfig.java
@@ -4,6 +4,7 @@ import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -24,8 +25,8 @@ public class KeycloakConfig extends KeycloakWebSecurityConfigurerAdapter {
 		return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
 	}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
 		keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
 		auth.authenticationProvider(keycloakAuthenticationProvider);
