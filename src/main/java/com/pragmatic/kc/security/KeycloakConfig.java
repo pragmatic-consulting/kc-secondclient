870c06591940a55b7e3905def222078d04bce4fa
Alae Es-saki <alaeessaki@gmail.com>
2021-08-05 12:30:13 +0100

    changing sec

--- src/main/java/com/pragmatic/kc/security/KeycloakConfig.java
+++ src/main/java/com/pragmatic/kc/security/KeycloakConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
@@ -15,9 +16,10 @@ import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-@KeycloakConfiguration
+//@KeycloakConfiguration
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class KeycloakConfig extends KeycloakWebSecurityConfigurerAdapter {
 
 	@Bean
